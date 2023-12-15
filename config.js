93899a2c6a493b45ca444acff973ccdd0a01b317
Yash Kalia <yashkalia4215@gmail.com>
2023-12-15 17:32:00 +0530

    finally?

---
+++ config.js
@@ -0,0 +1,5 @@
+export const PORT =5555;
+export const mongoDBURL=
+'mongodb+srv://admin:admin@cluster0.dps96zu.mongodb.net/books-collection?retryWrites=true&w=majority'
+//Please create a free database for yourself
+//mongodb+srv://admin:<password>@cluster0.dps96zu.mongodb.net/?retryWrites=true&w=majority
